8b00c7e4e287cb1b5d5a855c91b274e682ad9b32
Massimo Galleri <massimo.galleri@b-i.com>
2019-02-04 11:04:01 +0100

    lock circle position on hover

--- fancy-mouse.js
+++ fancy-mouse.js
@@ -1,27 +1,43 @@
-let cursor = document.getElementById("cursor");
-let dot = document.querySelector(".cursor-dot");
-let circle = document.querySelector(".cursor-circle");
+let cursor = document.getElementById('cursor');
+let dot = document.querySelector('.cursor-dot');
+let circle = document.querySelector('.cursor-circle');
 let link = document.querySelectorAll('[data-hover]');
+let lockedPos = true;
+let isStuck = false;
+let cursorPosX = 0;
+let cursorPosY = 0;
 
 let setCursorPosition = function (e) {
-    dot.style.transform = "translate(" + e.clientX + "px," + e.clientY + "px)";
-    circle.style.transform = "translate(" + e.clientX + "px," + e.clientY + "px)";
+    if (isStuck && lockedPos) {
+        cursorPosX = stuckX;
+        cursorPosY = stuckY;
+    } else {
+        cursorPosX = e.clientX;
+        cursorPosY = e.clientY;
+    }
+    dot.style.transform = 'translate(' + e.clientX + 'px,' + e.clientY + 'px)';
+    circle.style.transform = 'translate(' + cursorPosX + 'px,' + cursorPosY + 'px)';
 };
 
-document.addEventListener("mousemove", e => setCursorPosition(e));
+document.addEventListener('mousemove', e => setCursorPosition(e));
 
 link.forEach(function (item) {
-    item.addEventListener("mousedown", function (e) {
-        cursor.classList.add("cursor-down");
+    item.addEventListener('mousedown', function (e) {
+        cursor.classList.add('cursor-down');
     });
-    item.addEventListener("mouseup", function (e) {
-        cursor.classList.remove("cursor-down");
+    item.addEventListener('mouseup', function (e) {
+        cursor.classList.remove('cursor-down');
     });
-    item.addEventListener("mouseenter", function (e) {
-        cursor.classList.add("cursor-enter");
+    item.addEventListener('mouseenter', function (e) {
+        cursor.classList.add('cursor-enter');
+        const navItem = e.currentTarget;
+        const navItemBox = navItem.getBoundingClientRect();
+        stuckX = Math.round(navItemBox.left + navItemBox.width / 2);
+        stuckY = Math.round(navItemBox.top + navItemBox.height / 2);
+        isStuck = true;
     });
-    item.addEventListener("mouseleave", function (e) {
-        cursor.classList.remove("cursor-enter");
+    item.addEventListener('mouseleave', function (e) {
+        cursor.classList.remove('cursor-enter');
+        isStuck = false;
     });
 });
-
